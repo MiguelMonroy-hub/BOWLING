d08ec48c47d9bbf50a94e5fdd159974a8c7785cd
Miguel Angel Monroy Posada <A01739891@tec.mx>
2026-02-20 08:30:28 -0600

    "Spare_test y bowling"

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,9 +1,12 @@
 defmodule Bowling do
   def score([]), do: 0
-  def score(game) do
-    [frame|others] = game
-    [f,s|_] = frame
-    f + s + score(others)
-
+  def score([[f,s|_]|others]) do
+    if f + s == 10 do
+      nextframe = hd(others)
+      t = hd(nextframe)
+      f + s + t + score(others)
+    else
+      f + s + score(others)
+    end
   end
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -10,4 +10,8 @@ defmodule BowlingTest do
     game = List.duplicate([1, 1], 9) ++ [[1, 1, nil]]
     assert Bowling.score(game) == 20
   end
+    test "one spare" do
+    game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 16
+  end
 end
