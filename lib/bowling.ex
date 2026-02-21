132b91c16ae057cd4c36aec727838ee7b5e04587
Miguel Angel Monroy Posada <A01739891@tec.mx>
2026-02-20 23:26:36 -0600

    Modifición para el codigo con el test 5

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,12 +1,27 @@
 defmodule Bowling do
   def score([]), do: 0
-  def score([[f,s|_]|others]) do
-    if f + s == 10 do
-      nextframe = hd(others)
-      t = hd(nextframe)
-      f + s + t + score(others)
-    else
-      f + s + score(others)
-    end
+
+  def score([[10, b1, b2]]), do: 10 + b1 + b2
+
+  def score([[f, s, nil]]), do: f + s
+
+  def score([[f, _] | others]) when f == 10 do
+    10 + strike_bonus(others) + score(others)
   end
+
+  def score([[f, s] | others]) when f + s == 10 do
+    10 + spare_bonus(others) + score(others)
+  end
+
+  def score([[f, s] | others]) do
+    f + s + score(others)
+  end
+
+defp strike_bonus([[10, _] | [[f | _] | _]]), do: 10 + f
+
+defp strike_bonus([[10, b1, b2]]), do: b1 + b2
+
+defp strike_bonus([[f, s] | _]), do: f + s
+
+defp spare_bonus([[f | _] | _]), do: f
 end
--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -14,4 +14,14 @@ defmodule BowlingTest do
     game = [[5,5],[3,0]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
     assert Bowling.score(game) == 16
   end
+
+    test "one strike" do
+    game = [[10,nil],[3,4]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    assert Bowling.score(game) == 24
+  end
+
+  test "perfect game" do
+  game = List.duplicate([10,nil], 9) ++ [[10,10,10]]
+  assert Bowling.score(game) == 300
+  end
 end
